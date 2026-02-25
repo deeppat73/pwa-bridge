441834c967dd938a3c149dd1ec49ba9c3253dafc
deepchit <97021585+deeppat73@users.noreply.github.com>
2026-02-25 13:57:39 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'bridge-v1';
+const ASSETS = ['./index.html', './manifest.json','./icon-192x192.png','./icon-512x512.png'];
+
+// Cache files on install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Serve from cache if the network is down
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
