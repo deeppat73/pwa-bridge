7011b9ba2d7fd405ffdea57260605234930c9e50
deepchit <97021585+deeppat73@users.noreply.github.com>
2026-02-25 16:34:52 +0100

    Bypass cache for localhost requests in service worker
    Added logic to bypass caching for localhost requests.

--- sw.js
+++ sw.js
@@ -12,8 +12,15 @@ self.addEventListener('install', (event) => {
     );
 });
 
-// Serve from cache if the network is down
 self.addEventListener('fetch', (event) => {
+    // 1. Check if the request is going to localhost or 127.0.0.1
+    if (event.request.url.includes('localhost') || event.request.url.includes('127.0.0.1')) {
+        // Return nothing, which tells the browser to handle the request 
+        // normally OUTSIDE of the Service Worker's sandbox.
+        return; 
+    }
+
+    // 2. Otherwise, handle with standard caching logic
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
